c21c50df30bcac994cde6a2454ce3ca318181f99
Kavita Nambissan Ganguli <kavitanambissan@gmail.com>
2018-10-23 20:41:02 +0530

    Added all 4 multer methods

--- app/controller.js
+++ app/controller.js
@@ -4,30 +4,33 @@ module.exports.loadHome = (req, res) => {
 
 module.exports.handleMultiForm = (req, res) => {
   //To handle a multipart/form-data with multiple files
-  console.log(req.body);
-  console.log(req.files);
-  res.redirect("/form/multi");
+  console.log(req.body);    //Show text fields
+  console.log(req.files);   //Show array of file fields
+  res.redirect('/form/multi');
 };
 
 module.exports.handleBasicForm = (req, res) => {
+  //Handle basic text field only form
   // req.on('data', (data) => {
   //   console.log(data.toString());
   // });
 
   // req.on('end', () => {
-  //   res.send('ok');
+  //   res.send('Handled');
   // });
+  console.log('Request body : ');
   console.log(req.body);
+  res.redirect("/");
 };
 
 module.exports.handleSingleForm = (req, res) => {
   //Handle a single file upload
-  console.log(req.body);
-  console.log(req.file);
+  console.log(req.body);    //Show my text field
+  console.log(req.file);    //Show my file field
   res.redirect("/form/single");
 };
 
 module.exports.handleFormLoad = (req, res) => {
   let formType = req.params.type;
-  res.render(formType, {title: 'Form Page', message: 'Submit ' + formType});
+  res.render(formType, {title: 'Form Page'});
 }
--- app/routes.js
+++ app/routes.js
@@ -1,20 +1,22 @@
 const controller = require('./controller');
 const multer = require('multer');
-var upload = multer();
-var storage = multer.diskStorage({
+
+const upload = multer();
+
+const storage = multer.diskStorage({
   destination: function(req, file, cb){
-    cb(null, 'tmp/');
+    cb(null, 'uploads/')
   },
   filename: function(req, file, cb){
     cb(null, file.originalname + '-' + (new Date()).getDate());
   }
-})
-var uploadFiles = multer({storage: storage});
+});
+const uploadSingle = multer({storage: storage});
 
 module.exports = app => {
   app.get('/', controller.loadHome);
-  app.post('/multiform', uploadFiles.array('file1', 2), controller.handleMultiForm);
-  app.post('/basicform', upload.any(), controller.handleBasicForm);
-  app.post('/singleform', uploadFiles.single('file1'), controller.handleSingleForm);
+  app.post('/multiform', uploadSingle.fields([{name: 'file1', maxCount: 1}, {name: 'file2', maxCount: 1}]), controller.handleMultiForm);
+  app.post('/basicform', upload.none(), controller.handleBasicForm);
+  app.post('/singleform', uploadSingle.single('file1'), controller.handleSingleForm);
   app.get('/form/:type', controller.handleFormLoad)
 }
